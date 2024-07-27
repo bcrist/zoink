778ea783911ecd1afedf25da321879106691ea37
Ben Crist <github@magicmoremagic.com>
2024-07-27 13:41:26 -0500

    preview drawing

--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@
 * Are you tired of painstakingly entering your designs in KiCAD's schematic editor before you can lay out the board?
 * Have you ever wished that putting together a board design was less like photo editing and more like programming?
 * Do you hate manually creating KiCAD symbols and footprints for every part you use, but just can't bring yourself to trust the default KiCAD component libraries?
-* Are your wracked with disappointment when you send a board off to the fab only to notice that you forgot a decoupling cap somewhere?
+* Are you wracked with disappointment when you send a board off to the fab only to notice that you forgot a decoupling cap somewhere?
 * Do you wish KiCAD's ERC could check for logic level incompatibilities, bus contention, and behavioral correctness of your design?
 * Are you addicted to writing Zig code?
 
@@ -24,7 +24,7 @@ const zoink = @import("zoink");
 ```
 You can add parts to the board with `Board.part(type)`.  This will return a pointer to an instance of the part type that you passed in.  You can find built-in parts in `zoink.parts`, or create your own.  Part types must be able to be initialized from `.{}`.  You must assign nets to all the non-power pins of the part after adding it to the board.  Failing to do this will generate an error when trying to use the board later.
 
-You can use `Board.net(name)` to get or create a named net.  This returns a net ID that can be saved to a variable and used by value.  You can get or create a bus with `Board.bus(name, width)`.  A bus is just `[n]Net_ID` (or `[]const Net_ID`) so you can use zig's `++` operator to concatenate buses, and the normal slicing syntax to extract part of a bus.  You can also retrieve only part of the bus from the start with.  e.g. to get the high nibble of an 8-bit bus: `b.bus("MY_BUS[4:7]", 4)`.  Note that the length of the subscript must match the length parameter in this case.
+You can use `Board.net(name)` to get or create a named net.  This returns a net ID that can be saved to a variable and used by value.  You can get or create a bus with `Board.bus(name, width)`.  A bus is just `[n]Net_ID` (or `[]const Net_ID`) so you can use zig's `++` operator to concatenate buses, and the normal slicing syntax to extract part of a bus.  You can also tell `b.bus()` to retrieve only part of the bus.  For example, to get the high nibble of an 8-bit bus: `b.bus("MY_BUS[4:7]", 4)`.  Indexes are 0-based, and both endpoints are inclusive.  Swapping the endpoints will reverse the order of the bits in the resulting array.  Note that the length parameter passed to `b.bus()` must match the number of bits being extracted.
 
 ### Automatic Power Connection & Decoupling Caps
 Parts that contain a field named `pwr` will automatically have the signals in that struct connected to the power nets of the same name if they are not manually set.  Most parts will also automatically insert a decoupling capacitor for each non-ground signal.  This uses a special decoupling cap package, which has only 2 physical terminals, but 3 logical terminals.  This allows it to use a separate anonymous net to connect to the power pin, ensuring that the decoupling capacitor is actually placed right next to its associated power pin.
--- tools/preview.zig
+++ tools/preview.zig
@@ -2,49 +2,108 @@ const state = struct {
     var pass_action: sokol.gfx.PassAction = .{};
 };
 
+const font_data = @embedFile("font.ttf");
+
+var font: [*c]imgui.ImFont = null;
+
 export fn init() void {
-    // initialize sokol-gfx
     sokol.gfx.setup(.{
         .environment = sokol.glue.environment(),
         .logger = .{ .func = sokol.log.func },
     });
-    // initialize sokol-imgui
+    
     sokol.imgui.setup(.{
         .logger = .{ .func = sokol.log.func },
     });
 
-    // initial clear color
+
+    font = imgui.ImFontAtlas_AddFontFromMemoryTTF(imgui.igGetIO()[0].Fonts, @constCast(@ptrCast(font_data.ptr)), font_data.len, 18.5, null, null);
+
+    zgp.setup(.{
+        .allocator = std.heap.page_allocator,
+        .max_vertices = 1024 * 1024,
+    }) catch |err| {
+        log.err("Failed to set up ZGP: {}", .{ err });
+    };
+
     state.pass_action.colors[0] = .{
         .load_action = .CLEAR,
         .clear_value = .{ .r = 0.0, .g = 0.5, .b = 1.0, .a = 1.0 },
     };
 }
 
+var t: f64 = 0;
+
 export fn frame() void {
-    // call simgui.newFrame() before any ImGui calls
+    const width = sokol.app.width();
+    const height = sokol.app.height();
+
+    const w: f32 = @floatFromInt(width);
+    const h: f32 = @floatFromInt(height);
+    const aspect = w/h;
+
     sokol.imgui.newFrame(.{
-        .width = sokol.app.width(),
-        .height = sokol.app.height(),
+        .width = width,
+        .height = height,
         .delta_time = sokol.app.frameDuration(),
         .dpi_scale = sokol.app.dpiScale(),
     });
+    
+    zgp.begin(width, height) catch unreachable;
+    zgp.viewport(.{
+        .x = 0, .y = 0,
+        .w = width, .h = height,
+    }) catch unreachable;
+    zgp.project(-aspect, aspect, 1.0, -1.0);
+
+    // Draw an animated rectangle that rotates and changes its colors.
+    t += sokol.app.frameDuration() * 4;
+    const r: f32 = @floatCast(@sin(t)*0.5+0.5);
+    const g: f32 = @floatCast(@cos(t)*0.5+0.5);
+
+    zgp.set_color(.{ .r = 0, .g = 0, .b = 0 });
+    zgp.draw_lines_strip(&.{
+        .{ .x = 0.1, .y = 0 },
+        .{ .x = 0.7, .y = 1 },
+        .{ .x = 0.6, .y = -0.4 },
+        .{ .x = -0.5, .y = -0.3 },
+    }) catch unreachable;
+
+    zgp.push_transform() catch unreachable;
+    zgp.set_color(.{ .r = r, .g = g, .b = 0.3 });
+    zgp.rotate_at(@floatCast(t), 0.0, 0.0);
+    zgp.draw_filled_rect(-0.5, -0.5, 1.0, 1.0) catch unreachable;
+    zgp.pop_transform() catch unreachable;
+
+    zgp.set_color(.{ .r = 0, .g = 0, .b = 0 });
+    zgp.draw_lines_strip(&.{
+        .{ .x = 0, .y = 0 },
+        .{ .x = 0.5, .y = 0.5 },
+        .{ .x = 0.5, .y = -0.2 },
+        .{ .x = -0.4, .y = -0.2 },
+    }) catch unreachable;
+
 
     //=== UI CODE STARTS HERE
+    imgui.igPushFont(font);
     imgui.igSetNextWindowPos(.{ .x = 10, .y = 10 }, imgui.ImGuiCond_Once, .{ .x = 0, .y = 0 });
     imgui.igSetNextWindowSize(.{ .x = 400, .y = 100 }, imgui.ImGuiCond_Once);
     _ = imgui.igBegin("Hello Dear ImGui!", 0, imgui.ImGuiWindowFlags_None);
     _ = imgui.igColorEdit3("Background", &state.pass_action.colors[0].clear_value.r, imgui.ImGuiColorEditFlags_None);
+    imgui.igPopFont();
     imgui.igEnd();
     //=== UI CODE ENDS HERE
 
-    // call simgui.render() inside a sokol-gfx pass
     sokol.gfx.beginPass(.{ .action = state.pass_action, .swapchain = sokol.glue.swapchain() });
+    zgp.flush() catch unreachable;
+    zgp.end() catch unreachable;
     sokol.imgui.render();
     sokol.gfx.endPass();
     sokol.gfx.commit();
 }
 
 export fn cleanup() void {
+    zgp.shutdown();
     sokol.imgui.shutdown();
     sokol.gfx.shutdown();
 }
@@ -63,10 +122,15 @@ pub fn main() void {
         .window_title = "sokol-zig + Dear Imgui",
         .width = 800,
         .height = 600,
+        .sample_count = 4,
         .icon = .{ .sokol_default = true },
         .logger = .{ .func = sokol.log.func },
     });
 }
 
+const log = std.log.scoped(.main);
+
+const zgp = @import("zokol_gp.zig");
 const imgui = @import("imgui");
 const sokol = @import("sokol");
+const std = @import("std");
